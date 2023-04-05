d362594f245194afd01475aecde11efb4c348da1
Your Name <yassinbez111@GMAIL.COM>
2023-04-05 01:18:29 +0100

    ekhdem

---
+++ my-app-delev/src/Component/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Home() {
+  const backgroundImageUrl = 'https://family-images-y24bv7yxalct4.azureedge.net/families/9523/9523_background_1984x900.webp';
+
+  const styles = {
+    backgroundImage: `url(${backgroundImageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+
+
+
+    height: '600px'
+  };
+
+  return (
+
+    <div style={styles}>
+      <div className='absolute top-10 left-10'>
+        <div className='absolute top-48 left-10'>
+        <p className='font-bold text-white  bg text-lg'>ENJOY YOUR MEAL WITH FAST  <span className='text-yellow-300'>&& </span>YUMMY</p>
+        </div>
+       
+        <h1 className='text-1xl h-56 grid grid-cols-3 gap-4 content-center'></h1>
+        <span className=" font-bold box-decoration-clone  text-7xl red text-white px-8 ...">
+          Good food is  wise<br></br>
+          <span className='text-yellow-500 '>medicine</span>
+          <br></br>
+
+        </span>
+      
+        <button type="button" class=" m-8  text-white bg-yellow-600 hover:bg-yellow-500 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-8 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Order Food</button>
+      </div>
+
+    </div>
+
+
+
+  );
+}
+
+
+export default Home
